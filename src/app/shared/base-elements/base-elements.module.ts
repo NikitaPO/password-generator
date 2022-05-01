e00eb2a9a4e02d35776453be421ccaf8b976a91e
NikitaPO <gigzim345@gmail.com>
2022-05-02 00:48:04 +0300

    style: imports formatting

--- src/app/shared/base-elements/base-elements.module.ts
+++ src/app/shared/base-elements/base-elements.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {LinkComponent} from "./link/link.component";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+import {LinkComponent} from './link/link.component';
 
 
 @NgModule({
