3f437c75975be83fa9336ce96ae0a36922f559ed
NikitaPO <gigzim345@gmail.com>
2022-05-18 11:45:47 +0300

    feat: add custom form change by config when strength changed

--- src/app/components/password-generator/password-generator.component.ts
+++ src/app/components/password-generator/password-generator.component.ts
@@ -1,15 +1,18 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 
 import {PASSWORD_GENERATOR_CONFIG} from '../../constants';
 import {PasswordGeneratorConfig} from '../../types';
 
+
+@UntilDestroy()
 @Component({
   selector: 'app-password-generator',
   templateUrl: './password-generator.component.html',
   styleUrls: ['./password-generator.component.scss'],
 })
-export class PasswordGeneratorComponent {
+export class PasswordGeneratorComponent implements OnInit {
 
   linearSelectorItems: string[] = Object.keys(this.passwordGeneratorConfig.strengthTypes);
   minLength = this.passwordGeneratorConfig.minLength;
@@ -30,6 +33,12 @@ export class PasswordGeneratorComponent {
     private fb: FormBuilder,
     @Inject(PASSWORD_GENERATOR_CONFIG) private passwordGeneratorConfig: PasswordGeneratorConfig,
   ) {
+  }
+
+  ngOnInit() {
+    this.form.controls['strength'].valueChanges.pipe(untilDestroyed(this)).subscribe(strength => {
+      this.form.controls['custom'].setValue(this.passwordGeneratorConfig.strengthTypes[strength]);
+    })
   }
 
   asFormGroup = (a: AbstractControl): FormGroup => a as FormGroup;
